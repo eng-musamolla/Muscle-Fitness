7b12c7937aff48b74d42f409158af158d0775fd7
musamolla3131 <musamolla3131@gmail.com>
2022-09-29 23:51:41 +0600

    Everything is Ok!

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,11 +4,13 @@ const Card = ({ Activities, SelectButton }) => {
 
     const { name, img, detail, age, Time } = Activities;
 
+    const details = detail.slice(0, 100) + '...';
+
     return (
         <div className="overflow-hidden p-5 rounded-2xl border-solid xl:text-xl lg:text-gl md:text-md sm:text-sm  bg-white text-left ">
             <img src={img} alt='player img' className='w-100 rounded-lg'></img>
             <h3 className=" text-xl pt-4 font-bold  ">{name}</h3>
-            <p className="text-base my-2 text-gray-700">{detail}</p>
+            <p className="text-base my-2 text-gray-700">{details.length < 100 ? detail : details}</p>
             <p className="text-base">For Age: <span className='font-bold'>{age}</span></p>
 
             <p className="text-base">Time required: <span className='font-bold'>{Time} Minutes</span></p>
